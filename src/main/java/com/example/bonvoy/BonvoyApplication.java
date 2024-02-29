8c086dbb89a5c9a9ed6b2af1125faa466049464c
Nikhil-Navaneethan <nikhs059@gmail.com>
2024-02-29 16:54:35 +0530

    First Commit Backend

---
+++ src/main/java/com/example/bonvoy/BonvoyApplication.java
@@ -0,0 +1,13 @@
+package com.example.bonvoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonvoyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BonvoyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bonvoy/BonvoyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bonvoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BonvoyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
