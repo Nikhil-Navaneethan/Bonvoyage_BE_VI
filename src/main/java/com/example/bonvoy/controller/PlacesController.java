607fa0e6af733c5e8eae58755b942f1b4f9454aa
Nikhil-Navaneethan <nikhs059@gmail.com>
2024-03-13 12:34:29 +0530

    Midway commit  1

---
+++ src/main/java/com/example/bonvoy/controller/PlacesController.java
@@ -0,0 +1,62 @@
+package com.example.bonvoy.controller;
+
+import java.io.File;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+import com.example.bonvoy.model.Places;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.bonvoy.Repo.PlacesRepo;
+
+@RestController
+@RequestMapping("/places")
+public class PlacesController {
+@Autowired
+private PlacesRepo placesRepo;
+
+@PostMapping("/upload")
+public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile uploadedFile) {
+    if (uploadedFile.isEmpty()) {
+        return new ResponseEntity<>("Please select a file!", HttpStatus.OK);
+    }
+    try {
+
+        byte[] bytes = uploadedFile.getBytes();
+
+        UUID uuid = UUID.randomUUID();
+//        String uploadsLocation = env.getProperty("resource.uploads");
+        String uploadsLocation = "C:\\Users\\nikhs\\Desktop\\BonVoyage\\bonvoy\\src\\main\\resources\\Uploads\\";
+        String fileLocation = uploadsLocation + uuid + uploadedFile.getOriginalFilename();
+        Path path = Paths.get(fileLocation);
+        Files.write(path, bytes);
+
+        File file = new File(fileLocation);
+        return ResponseEntity.status(HttpStatus.OK).body(file.getName());
+
+    } catch (Exception e) {
+        e.printStackTrace();
+        return ResponseEntity.status(HttpStatus.CREATED).body(e.getMessage());
+
+    }
+}
+
+@PostMapping("/upload1")
+public ResponseEntity<?> Places(@RequestBody Places places){
+	Places savedEntity = placesRepo.saveAndFlush(places);
+			return ResponseEntity
+					.status(HttpStatus.OK)
+					.body(places);
+}
+
+}
---
+++ src/main/java/com/example/bonvoy/model/Places.java
@@ -0,0 +1,50 @@
+package com.example.bonvoy.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="places")
+public class Places {
+@Id
+@GeneratedValue
+private int Id;
+private String image;
+private String name;
+private String description;
+private String category;
+public int getId() {
+	return Id;
+}
+public void setId(int id) {
+	Id = id;
+}
+public String getImage() {
+	return image;
+}
+public void setImage(String image) {
+	this.image = image;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getDescription() {
+	return description;
+}
+public void setDescription(String description) {
+	description = description;
+}
+public String getCategory() {
+	return category;
+}
+public void setCategory(String category) {
+	this.category = category;
+}
+
+
+}
